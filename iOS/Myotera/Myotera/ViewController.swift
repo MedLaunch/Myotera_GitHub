51d8b9d849018c85f9f0a9fa3116a9357b957565
sahilG76 <sahilgu@umich.edu>
2020-10-26 22:00:42 -0400

    initial xcode proj added

---
+++ iOS/Myotera/Myotera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Myotera
+//
+//  Created by Sahil Gupta on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
