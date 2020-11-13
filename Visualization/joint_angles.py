d53867be629fb25084117e3ed25f1cc92111c58b
dnjn <dnjn@umich.edu>
2020-11-13 18:19:07 -0500

    update joint angles

--- Visualization/joint_angles.py
+++ Visualization/joint_angles.py
@@ -23,7 +23,18 @@
 #g2 = g2_prime_num / g2_prime_denom
 
 ##Set stuff
+#No idea
 
 ##Calculate unit-length direction vectors
 #j1 = (cos(phi_1)*cos(theta_1),cos(phi_1)*sin(theta_1),sin(phi_1))^transpose
 #j2 = (cos(phi_2)*cos(theta_2),cos(phi_2)*sin(theta_2),sin(phi_2))^transpose
+
+##Initialize orientation vectors for given times
+#o1 = ?
+#o2 = ?
+
+##Make new value Eta for radial and tangential acceleration
+#eucledian_norm(a1-Eta_g1) - eucledian_norm(a2-Eta_g2) = 0
+
+#Eta_g1 = g1 cross (g1 cross o1) + g1_prime cross o1
+#Eta_g2 = g2 cross (g2 cross o2) + g2_prime cross o2
