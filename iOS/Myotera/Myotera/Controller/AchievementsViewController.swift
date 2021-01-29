644ed556fc1afa60e1d5db7ae4b577dd3e47dbc2
sahilG76 <sahilgu@umich.edu>
2021-01-28 17:08:40 -0800

    up to Design Review 2

---
+++ iOS/Myotera/Myotera/Controller/AchievementsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AchievementsViewController.swift
+//  Myotera
+//
+//  Created by Sahil Gupta on 12/4/20.
+//
+
+import UIKit
+
+class AchievementsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/Myotera/Myotera/Controller/ProgressViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProgressViewController.swift
+//  Myotera
+//
+//  Created by Sahil Gupta on 12/3/20.
+//
+
+import UIKit
+
+class ProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
