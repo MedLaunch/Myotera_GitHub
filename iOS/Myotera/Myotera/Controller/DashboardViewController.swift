7eda8e6c59d9ced86d5b880896ff3132fda2bd11
sahilG76 <sahilgu@umich.edu>
2021-02-20 00:28:37 -0800

    piechart

--- iOS/Myotera/Myotera/Controller/DashboardViewController.swift
+++ iOS/Myotera/Myotera/Controller/DashboardViewController.swift
@@ -6,16 +6,54 @@
 //
 
 import UIKit
+import Charts
 
 class DashboardViewController: UIViewController {
     
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var pieChart: PieChartView!
     
+    let movementEntities:[String] = ["%Left", "%Right"]
+    let goals:[Double] = [59, 41]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        customizeChart(dataPoints: movementEntities, values: goals)
+    }
+    
+    func customizeChart(dataPoints: [String], values: [Double]) {
+        // TO-DO: customize the chart here
+        // 1. Set ChartDataEntry
+        var dataEntries: [ChartDataEntry] = []
+        for i in 0..<dataPoints.count {
+            let dataEntry = PieChartDataEntry(value: values[i], label: dataPoints[i], data: dataPoints[i] as AnyObject)
+            dataEntries.append(dataEntry)
+        }
+        // 2. Set ChartDataSet
+        let pieChartDataSet = PieChartDataSet(entries: dataEntries, label: nil)
+        pieChartDataSet.colors = colorsOfCharts(numbersOfColor: dataPoints.count)
+        // 3. Set ChartData
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        let format = NumberFormatter()
+        format.numberStyle = .none
+        let formatter = DefaultValueFormatter(formatter: format)
+        pieChartData.setValueFormatter(formatter)
+        // 4. Assign it to the chart’s data
+        pieChart.data = pieChartData
     }
 
+    private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
+      var colors: [UIColor] = []
+      for _ in 0..<numbersOfColor {
+        let red = Double(arc4random_uniform(256))
+        let green = Double(arc4random_uniform(256))
+        let blue = Double(arc4random_uniform(256))
+        let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+        colors.append(color)
+      }
+      return colors
+    }
 
 }
 
---
+++ iOS/Myotera/Myotera/Controller/RecordingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RecordingViewController.swift
+//  Myotera
+//
+//  Created by Sahil Gupta on 2/20/21.
+//
+
+import UIKit
+
+class RecordingViewController: UIViewController {
+    
+    @IBOutlet weak var recordingOrError: UILabel!
+    
+    var sensorsGood = true;
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if (sensorsGood) {
+            recordingOrError.text = "Recording..."
+        }
+        else {
+            recordingOrError.text = "Not enough sensors connected!"
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
